b86270ddda90dd7676cb71fe8bb789a08992e9a3
Valentin <valentin.linhardt@gmail.com>
2020-12-16 16:26:00 +0100

    - undone some things

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -22,16 +15,16 @@ public class Main  extends Application {
      * */
     Pane root;
 
+    // Statische Mandelbrotmenge
     private Parent createContent(){
 
         root = new Pane();
         Controller con = new Controller();
 
         con.makeMap();
-
         root.getChildren().add(con.map.mapGroup);
 
-        root = con.getEvents(root);
+        //root = con.getEvents(root);
 
         return root;
     }
@@ -39,13 +32,14 @@ public class Main  extends Application {
     @Override
     public void start(Stage primaryStage){
 
-        primaryStage.setScene(new Scene(createContent()));
-
         primaryStage.setHeight(Properties.WINDOW_HEIGHT+37);
         primaryStage.setWidth(Properties.WINDOW_WIDTH+13);
         primaryStage.setTitle(Properties.WINDOW_TITLE);
 
+        primaryStage.setScene(new Scene(createContent()));
         primaryStage.show();
+
+
 
     }
 }
