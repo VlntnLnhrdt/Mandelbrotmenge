f038f39a8107b060d31b293bc27d8b667459b397
Valentin <valentin.linhardt@gmail.com>
2021-01-17 23:10:00 +0100

    - completed

--- src/Pixel.java
+++ src/Pixel.java
@@ -2,25 +2,37 @@ import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+/**
+ *
+ * Dient zur Darstellung eines eingefärbten Pixels
+ * Diesem werden Höhe, Breite, x- sowie y-Position und die Farbe zugewiesen
+ *
+ * */
+
 public class Pixel {
 
-    /**
-    *
-    * Stellt einen Pixel dar
-    *
-    */
-
+    // Der wirkliche Pixel
     private final Node rectangle;
 
+    // Konstruktur des Pixels / Erzeugung und Positionierung des Pixels
     Pixel(int x, int y) {
         rectangle = new Rectangle(x, y, 1, 1); // Node mit den Eigenschaften eines Rectangles wird erstellt
     }
 
+    // Farbfestlegung des Pixels
     public void setColor(Color color) {
-        ((Rectangle)rectangle).setFill(color); // Farbgebung
+        ((Rectangle)rectangle).setFill(color);
     }
 
+    // Getter-Funktion für die Abrufung des Pixels
     public Node getRectangle() {
-        return rectangle;
+        try {
+            return rectangle;
+        } catch (Exception e) {
+            System.err.println("Eine Exception (vermutlich NullPointerException) ist aus bislang ungeklärter Ursache aufgetreten.");
+            System.err.println("Bitte starten Sie das Programm neu.");
+            System.exit(2020);
+        }
+        return null;
     }
 }
