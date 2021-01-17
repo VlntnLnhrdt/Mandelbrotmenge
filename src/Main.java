d1e5e4d4d03fc7a9c2dd8db81c36f76cf76cbc0f
Valentin <valentin.linhardt@gmail.com>
2021-01-17 23:37:26 +0100

    - completed

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Parent;
@@ -8,44 +11,67 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ *
+ * Start- und Hauptklasse des Programmes und dient zur Anzeige des konkreten Fensters
+ *
+ * */
 
 public class Main  extends Application {
 
-    /**
-     * Nur zur genrellen Darstellung eines Fensters und Starten des Programmes
-     */
-
+    // Inhalt des Fensters
     Pane root;
-    Controller con;
 
+    // Objekt, in welchem die Karte / das Gitter mit den Pixeln berechnet wird
+    Map map;
 
+    // Als "Hochzähler" für die wachsende Mandelbrot-Menge
     int growingIteration = 0;
-    Timer t;
+
+    // Timeline, Code wird basierend auf einer vorgegebenen Zeit regelmäßig durchgeführt (Für wachsende Mandelbrot-Menge)
+    Timeline growingTimeline;
 
     // Statische Mandelbrotmenge
     private Parent createContent() {
 
-        root = new Pane();
-        con = new Controller();
+        growingTimeline = new Timeline(new KeyFrame(Duration.millis(2000), e -> {
+            growingIteration = (growingIteration < 30) ? growingIteration + 1 : growingIteration * 2;
+            Properties.ITERATIONS = growingIteration;
 
-        con.makeMap();
-        root.getChildren().add(con.map.mapGroup);
+            System.out.println("Generating Map with " + growingIteration + " Iterations ...");
+
+            map.generateMap();
+        }));
+
+        growingTimeline.setCycleCount(Animation.INDEFINITE);
+        growingTimeline.stop();
+
+        root = new Pane();
+        map = new Map(Properties.WINDOW_HEIGHT, Properties.WINDOW_WIDTH);
+        map.generateMap();
+
+        root.getChildren().add(map.mapGroup);
 
         return root;
     }
 
+    // Erste Funktion, welche ausgeführt wird
     @Override
     public void start(Stage primaryStage) {
 
+        // Festlegung der Fenstergröße (die beiden Zahlen, welche addiert werden, beheben einen Anzeigefehler)
         primaryStage.setHeight(Properties.WINDOW_HEIGHT + 37);
         primaryStage.setWidth(Properties.WINDOW_WIDTH + 13);
+
+        // Festlegung des Fenstertitels
         primaryStage.setTitle(Properties.WINDOW_TITLE);
 
-        /* XX */ // Schließen des Fensters
+        // Sobald am Rechten-Oberen-Eck des Fensters des X angeklickt wird, erscheint folgende Benachrichtigung
         primaryStage.setOnCloseRequest(event -> {
             System.out.println("Berechnungen werden abgebrochen.");
             System.out.println("Fenster wird geschlossen.");
@@ -53,81 +79,43 @@ public class Main  extends Application {
             System.exit(0);
         });
 
-
+        // Setzt inhalt in Fenster und ruft obrige Funktion auf
         primaryStage.setScene(new Scene(createContent()));
 
-        /* XX */ // baldiger Zoom
+        // Dient der Vergrößerung der Mandelbrot-Menge und erfasst bestimmte Mauseingaben
         root.setOnMouseClicked(mouseEvent -> {
+            // Wenn die Linke-Maustaste gedrückt wird, werden die Positionsdaten an das Objekt "Map" übergeben
             if (mouseEvent.getButton() == MouseButton.PRIMARY) {
-                if (!con.map.growing)
-                    con.map.generateMap((int) mouseEvent.getX(), (int) mouseEvent.getY());
+                // Wird nur durchgeführt, wenn die Mandelbrot-Menge aktuell nicht wächst (überschneidet sich sonst und es führt zu Anzeigeproblemen)
+                if (!map.growing)
+                    map.generateMap((int) mouseEvent.getX(), (int) mouseEvent.getY());
             }
 
+            // Wenn die Rechte-Maustaste gedrückt wird, wird die Mandelbrot-Menge wieder zur Normalgröße zurückgesetzt
             if (mouseEvent.getButton() == MouseButton.SECONDARY) {
-                con.map.zoomed = false;
-                con.map.generateMap(0,0);
+                map.zoomed = false;
+                map.generateMap(0,0);
             }
         });
 
-        /* XX */ // Wenn eine Taste gedrückt wird
+        // Dient des "Wachsens" der Mandelbrot-Menge und erfasst bestimmte Tasteneingaben
         primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, e -> {
-            System.out.println("Taste: " + e.getCode().toString());
-            if (e.getCode() == KeyCode.ENTER) { // Bei Enter wird das Folgende ausgeführt
-                con.map.growing = true;
-                if (!con.map.zoomed) { // Wenn gezoomt ist werden Tasten ignoriert
-                    if (!Properties.GROWING) {
-                        TimerTask tt = new TimerTask() {
-                            @Override
-                            public void run() {
-                                /* XX */
-                                growingIteration = (growingIteration < 30) ? growingIteration + 1 : growingIteration * 2;
-                                Properties.ITERATIONS = growingIteration;
-
-                                System.out.println("Generating Map with " + growingIteration + " Iterations ...");
-
-                                try {
-                                    con.map.generateMap();
-                                } catch (Exception e) {
-                                    System.err.println("Eine Exception (vermutlich NullPointerException) ist aus bislang ungeklärter Ursache aufgetreten.");
-                                    System.err.println("Bitte starten Sie das Programm neu.");
-                                    System.exit(2020);
-                                }
-
-                            }
-                        };
-
-                        /* XX */
-                        t = new Timer();
-                        t.schedule(tt, 0, 1000);
-                    } else {
-                        t.cancel();
-                    }
-
+            // Wenn Enter gedrückt wurde, startet oder stoppt das Wachstum der Mandelbrot-Menge
+            if (e.getCode() == KeyCode.ENTER) {
+                // Wenn zuvor bereits vergrößert wurde, wird die Tasteneingabe ignoriert (überschneidet sich sonst und es führt zu Anzeigeproblemen)
+                if (!map.zoomed) {
+                    // Wachstumsstatus wird umgedreht
                     Properties.GROWING = !Properties.GROWING;
+
+                    // Wenn das Wachstum bereits läuft, wird es gestoppt, ansonsten anderes herum
+                    if (growingTimeline.getStatus() == Animation.Status.RUNNING)
+                        growingTimeline.stop();
+                    else
+                        growingTimeline.play();
                 }
             }
-
-            /* XX */
-            if (e.getCode() == KeyCode.UP && !Properties.GROWING) { // Wenn Pfeiltaste + Iteration wächst aktuell nicht
-                Properties.ITERATIONS = growingIteration = (growingIteration < 30) ? growingIteration + 1 : growingIteration * 2;
-
-                System.out.println("Generating Map with " + growingIteration + " Iterations ...");
-
-                con.map.generateMap();
-            }
-
-            if (e.getCode() == KeyCode.DOWN && !Properties.GROWING && growingIteration > 1) { // Wenn Pfeiltaste + Iteration wächst aktuell nicht
-                Properties.ITERATIONS = growingIteration = (growingIteration <= 30) ? growingIteration - 1 : growingIteration / 2;
-                ;
-
-                System.out.println("Generating Map with " + growingIteration + " Iterations ...");
-
-                con.map.generateMap();
-            }
         });
-
+        // Zeigt das Fenster dann wirklich an (hierdurch erscheint es erst)
         primaryStage.show();
-
-
     }
 }
