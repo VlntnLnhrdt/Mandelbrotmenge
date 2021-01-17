0d0d944e0de36bf115179150250ede77090bf0c8
Valentin <valentin.linhardt@gmail.com>
2021-01-17 23:04:11 +0100

    - completed

--- src/Properties.java
+++ src/Properties.java
@@ -1,25 +1,34 @@
+/**
+ *
+ * Enthält Daten zur besseren Übersicht und einfachereren Veränderung
+ * Kann wie eine Art Konfigurationsdatei für bestimmte Einstellungen angesehen werden
+ *
+ * */
+
 public class Properties {
 
-    /**
-     *
-     * Enthält Daten (zur besseren Übersicht und einfachereren Veränderung)
-     *
-     * */
+    // Bezeichnung des Fenstertitels
+    final public static String WINDOW_TITLE = "Linhardt, Valentin 2021 - Seminararbeit: Die Mandelbrot-Menge als mathematisches und künstlerisches Objekt";
 
-    final public static String WINDOW_TITLE = "Linhardt, Valentin 2020 - Seminararbeit: Die Mandelbrotmenge als mathematisches und künstlerisches Objekt";
-
-    final public static int WINDOW_HEIGHT = 1000; // !! Nicht verändern !!
+    // Einstellung der Fenstergröße
+    final public static int WINDOW_HEIGHT = 1000;
     final public static int WINDOW_WIDTH = WINDOW_HEIGHT;
+
+    // Dient zur Positionierung der Mandelbrot-Menge bei erster Anzeige
     public static int WINDOW_CENTER_HOR = (WINDOW_WIDTH/5)*4; // Mehr Punkte in negativer Richtigung der reelen Zahlen
     public static int WINDOW_CENTER_VER = WINDOW_HEIGHT/2;    // Genau mittig (imaginäre Zahlen ausgeglichen)
 
+    // Einstellung der Genauigkeit / Schärfe, je mehr Iterationen desto genauer
+    public static long ITERATIONS = 1000;
 
-    public static long ITERATIONS = 1000; // Anzahl der zu durchlaufenen Iterationen (je mehr desto "schärfer")
+    // Verhältnis zwischen Pixel und Längeeinheit der Achsen
     public static double REAL_LENGTH = 2.5; // Definiert die insgesamte Länge der Koordiantenachse(n) (-2 bis 0,5 ist 2,5)
     public static double SCALE = Properties.REAL_LENGTH / Properties.WINDOW_WIDTH;
 
-    public static int ZOOM_FACTOR = 2;
+    // Zoomfaktor bei Vergrößerung
+    public static int ZOOM_FACTOR = 5;
 
+    // Aktuelles Status, ob die Mandelbrot-Menge gerade "wächst"
     public static boolean GROWING = false;
 
 }
